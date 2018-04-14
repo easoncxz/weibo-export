600aca9c358f9fbba57e5163b827204ce8428f6c
Eason Chen <easoncxz@gmail.com>
2018-04-15 00:44:56 +1200

    Expose mtl-style API for picture download

--- src/API/Client.hs
+++ src/API/Client.hs
@@ -5,6 +5,7 @@ module API.Client
   ( Cookie(..)
   , WeiboApiClient
   , WeiboApiM
+  , downloadPicture
   , getComments
   , getStatuses
   , largeJpgUrl
@@ -15,15 +16,18 @@ module API.Client
 import Control.Lens
 import Control.Monad.Except
 import Control.Monad.Reader
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.UTF8 as BS8
 import Data.Monoid ((<>))
 import Data.Proxy
+import qualified Data.Sequence as Seq
 import Data.Text
 import qualified Data.Text.Encoding as T
 import Network.HTTP.Client (newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Network.HTTP.Types.URI as HTTP
 import qualified Network.Wreq as Wreq
+import qualified Network.Wreq.Lens as Wreq
 import Servant.API
 import Servant.Client
 
@@ -44,33 +48,20 @@ type WeiboApi = Header "Cookie" Cookie :> (StatusesApi :<|> CommentsApi)
 weiboApi :: Proxy WeiboApi
 weiboApi = Proxy
 
-largeJpgUrl :: PictureID -> String
-largeJpgUrl (ID pid) =
-  BS8.toString $
-  "https://ww1.sinaimg.cn/large/" <> HTTP.urlEncode True (T.encodeUtf8 pid) <>
-  ".jpg"
-
 data WeiboApiClient = WeiboApiClient
   { weiboApiGetStatuses :: Maybe Int -> IO (Either ServantError StatusListResponse)
   , weiboApiGetComments :: StatusID -> Maybe Int -> IO (Either ServantError CommentListResponse)
-  , weiboApiDownloadPicture :: PictureID -> IO Picture
   }
 
 newWeiboApiClient :: Cookie -> IO WeiboApiClient
 newWeiboApiClient cookie = do
   settings <- newManager tlsManagerSettings
   let clientEnv = mkClientEnv settings (BaseUrl Https "m.weibo.cn" 443 "")
-  let getStatusesM :<|> getCommentsM = client weiboApi (Just cookie)
+      getStatusesM :<|> getCommentsM = client weiboApi (Just cookie)
       weiboApiGetStatuses mbPage =
         runClientM (getStatusesM (Just "cards") mbPage) clientEnv
       weiboApiGetComments statusID mbPage =
         runClientM (getCommentsM (Just statusID) mbPage) clientEnv
-      weiboApiDownloadPicture pid =
-        liftIO $ do
-          r <- Wreq.get (largeJpgUrl pid)
-          let _pictureIdentifier = pid
-              _pictureBytes = Just (r ^. Wreq.responseBody)
-          return Picture {..}
   return WeiboApiClient {..}
 
 type WeiboApiM = ExceptT ServantError (ReaderT WeiboApiClient IO)
@@ -95,3 +86,28 @@ getComments statusID mbPage = do
 
 runWeiboClientM :: WeiboApiM a -> WeiboApiClient -> IO (Either ServantError a)
 runWeiboClientM = runReaderT . runExceptT
+
+largeJpgUrl :: PictureID -> String
+largeJpgUrl (ID pid) =
+  BS8.toString $ "https://ww1.sinaimg.cn/large/" <>
+  HTTP.urlEncode True (T.encodeUtf8 pid) <>
+  ".jpg"
+
+servantResponseFromWreq :: Wreq.Response BSL.ByteString -> Response
+servantResponseFromWreq wr =
+  Response
+    { responseStatusCode = wr ^. Wreq.responseStatus
+    , responseHeaders = Seq.fromList (wr ^. Wreq.responseHeaders)
+    , responseHttpVersion = wr ^. Wreq.responseVersion
+    , responseBody = wr ^. Wreq.responseBody
+    }
+
+downloadPicture ::
+     (MonadIO m, MonadError ServantError m) => PictureID -> m Picture
+downloadPicture pid = do
+  r <- liftIO $ Wreq.get (largeJpgUrl pid)
+  when (r ^. Wreq.responseStatus . Wreq.statusCode /= 200) $ do
+    throwError . FailureResponse . servantResponseFromWreq $ r
+  let _pictureIdentifier = pid
+      _pictureBytes = Just (r ^. Wreq.responseBody)
+  return Picture {..}
