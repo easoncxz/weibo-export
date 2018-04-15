a48a406408ee7c8973bc33aca474efa039489ec7
Eason Chen <easoncxz@gmail.com>
2018-04-15 15:50:09 +1200

    Revert "Add leniency in format of RetweetedStatus"
    This reverts commit 15806d2be28c389af96d7a68b5c02418e9005bd1.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,4 @@
 module Main where
 
-import qualified Data.Text.IO as T
-import Servant.Client (ServantError)
-
-import API.Client
-import Downloader
-
-main :: IO (Either ServantError [DeepStatus])
-main = do
-  cookieT <- T.getLine
-  client <- newWeiboApiClient (Cookie cookieT)
-  runWeiboClientM downloadEverything client
+main :: IO ()
+main = return ()
